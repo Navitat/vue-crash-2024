330bddd999ff9e66722330470e554dcc81a9f24c
navitat <anlon636@gmail.com>
2025-09-17 12:06:58 +0200

    refactor: move optionsAPI code to app-options..., left app.vue for compositionAPI

--- src/App-optionsAPI.vue
+++ src/App-optionsAPI.vue
@@ -1,5 +1,25 @@
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      name: 'Andres Felipe',
+      status: 'pending',
+      tasks: ['Task One', 'Task Two', 'Task Three'],
+      link: 'https://google.com',
+    }
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending'
+      } else if (this.status === 'pending') {
+        this.status = 'inactive'
+      } else {
+        this.status = 'active'
+      }
+    },
+  },
+}
 </script>
 
 <template>
