da3801b02b78c37344f6c72d3c28e9255cd930a8
navitat <anlon636@gmail.com>
2025-09-17 15:14:03 +0200

    add show more or less description functionality

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -1,8 +1,23 @@
 <script setup>
-import { defineProps } from 'vue'
+import { defineProps, ref, computed } from 'vue'
 
-defineProps({
+const props = defineProps({
   job: Object,
+})
+
+const showFullDescription = ref(false)
+
+const toggleFullDescription = () => {
+  showFullDescription.value = !showFullDescription.value
+}
+
+const truncatedDescription = computed(() => {
+  let description = props.job.description;
+  if (!showFullDescription.value) {
+    description = description.substring(0, 90) + "..."
+  }
+
+  return description;
 })
 </script>
 
@@ -15,7 +30,12 @@ defineProps({
       </div>
 
       <div class="mb-5">
-        {{ job.description }}
+        <div>
+          {{ truncatedDescription }}
+        </div>
+        <button @click="toggleFullDescription" class="text-green-500 hover:text-green-600 mb-5">
+          {{ showFullDescription ? "Less" : "More" }}
+        </button>
       </div>
 
       <h3 class="text-green-500 mb-2">{{ job.salary }} / Year</h3>
@@ -27,10 +47,8 @@ defineProps({
           <i class="fa-solid fa-location-dot text-lg"></i>
           {{ job.location }}
         </div>
-        <a
-          :href="'/job/' + job.id"
-          class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm"
-        >
+        <a :href="'/job/' + job.id"
+          class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm">
           Read More
         </a>
       </div>
