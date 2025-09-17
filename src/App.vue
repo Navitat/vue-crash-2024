eb4a95d6e6662cc98df7cc4f1948c11bbe4d0bf3
navitat <anlon636@gmail.com>
2025-09-17 11:55:55 +0200

    feat: ielse and for in

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'Andres Felipe',
+      status: 'pending',
+      tasks: ['Task One', 'Task Two', 'Task Three'],
+    }
+  },
+}
+</script>
+
 <template>
-  <h1>Vue Jobs</h1>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else="status">User is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
 </template>
