a515680b7c73a56be32537eccc535240e489f872
navitat <anlon636@gmail.com>
2025-09-17 12:02:07 +0200

    feat: toggle and button example

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,19 @@ export default {
       name: 'Andres Felipe',
       status: 'pending',
       tasks: ['Task One', 'Task Two', 'Task Three'],
+      link: 'https://google.com',
     }
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending'
+      } else if (this.status === 'pending') {
+        this.status = 'inactive'
+      } else {
+        this.status = 'active'
+      }
+    },
   },
 }
 </script>
@@ -20,4 +32,9 @@ export default {
   <ul>
     <li v-for="task in tasks" :key="task">{{ task }}</li>
   </ul>
+  <!-- <a v-bind:href="link">Click for Google</a> -->
+  <a :href="link" target="_blank">Click for Google</a>
+  <br />
+  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
+  <button @click="toggleStatus">Change Status</button>
 </template>
